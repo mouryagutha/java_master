dac1aaa1f6e6a6803275d7dbc2bf8a775b82e9b9
mourya kumar <kmouryap@gmail.com>
2024-01-31 16:32:55 +0530

    Search_method

---
+++ src/arrays/Search_method.java
@@ -0,0 +1,27 @@
+package arrays;
+
+
+class Search_method {
+    static int findelement(int[] arr, int n , int key)
+    {
+        for(int i = 0; i<n;i++)
+        {
+            if(arr[i]== key)
+                return i;
+        }
+        return -1;
+    }
+    public static  void main(String[] args){
+        int arr[] = { 12,34,10,6, 40};
+        int n = arr.length;
+        int key = 40 ;
+
+        int posistion = findelement(arr,n, key);
+        if(posistion == -1)
+            System.out.println("Element not found");
+        else
+            System.out.println("Element found at postion:"+(posistion+1));
+
+
+    }
+}
