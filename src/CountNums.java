417da24bc70c8381a96c6f87a7766382ee47646e
mourya kumar <kmouryap@gmail.com>
2024-02-09 00:06:07 +0530

    2-9-24

---
+++ src/CountNums.java
@@ -0,0 +1,19 @@
+public class CountNums {
+    public static  void main (String ars[])
+    {
+        int n = 243;
+
+        int count = 0;
+        while(n>0)
+        {
+            int rem = n % 10;
+            if(rem == 5)
+            {
+                count++;
+            }
+            n= n/10;
+        }
+        System.out.println(count);
+    }
+
+}
---
+++ src/FindLargest.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FindLargest {
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+
+        int max = Math.max(c,Math.max(a,b));
+
+        System.out.println(max);
+    }
+}
