5a2532797c2bc4a773bc37c88962d4dfc21d04f7
mourya kumar <kmouryap@gmail.com>
2024-02-09 00:15:40 +0530

    reserve

--- src/Reverse.java
+++ src/Reverse.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Reverse {
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println(" enter the value ");
+        int num = in.nextInt();
+
+        int ans = 0 ;
+
+        while (num> 0)
+        {
+            int rem = num % 10 ;
+            num/= 10;
+
+            ans = ans * 10 + rem;
+        }
+        System.out.println(ans);
+    }
 }
