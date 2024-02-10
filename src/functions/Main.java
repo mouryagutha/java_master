3aeee4484a50239caac06e5ca0bfe1af36885c2d
mourya kumar <kmouryap@gmail.com>
2024-02-10 16:27:19 +0530

    sum

--- src/functions/Main.java
+++ src/functions/Main.java
@@ -1,2 +1,92 @@
-package functions;public class Main {
+package functions;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static  void  main (String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int num1, num2, sum;
+
+        System.out.print("Enter number 1: ");
+        num1 = in.nextInt();
+        System.out.print("Enter number 2: ");
+        num2 = in.nextInt();
+        sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+
+        System.out.print("Enter number 1: ");
+        num1 = in.nextInt();
+        System.out.print("Enter number 2: ");
+        num2 = in.nextInt();
+        sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+
+        System.out.print("Enter number 1: ");
+        num1 = in.nextInt();
+        System.out.print("Enter number 2: ");
+        num2 = in.nextInt();
+        sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+
+        System.out.print("Enter number 1: ");
+        num1 = in.nextInt();
+        System.out.print("Enter number 2: ");
+        num2 = in.nextInt();
+        sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+
+        System.out.print("Enter number 1: ");
+        num1 = in.nextInt();
+        System.out.print("Enter number 2: ");
+        num2 = in.nextInt();
+        sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+
+        System.out.print("Enter number 1: ");
+        num1 = in.nextInt();
+        System.out.print("Enter number 2: ");
+        num2 = in.nextInt();
+        sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+
+
+        System.out.print("Enter number 1: ");
+        num1 = in.nextInt();
+        System.out.print("Enter number 2: ");
+        num2 = in.nextInt();
+        sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+
+        System.out.print("Enter number 1: ");
+        num1 = in.nextInt();
+        System.out.print("Enter number 2: ");
+        num2 = in.nextInt();
+        sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+
+        System.out.print("Enter number 1: ");
+        num1 = in.nextInt();
+        System.out.print("Enter number 2: ");
+        num2 = in.nextInt();
+        sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+
+        System.out.print("Enter number 1: ");
+        num1 = in.nextInt();
+        System.out.print("Enter number 2: ");
+        num2 = in.nextInt();
+        sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+
+
+        System.out.print("Enter number 1: ");
+        num1 = in.nextInt();
+        System.out.print("Enter number 2: ");
+        num2 = in.nextInt();
+        sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+
+    }
 }
