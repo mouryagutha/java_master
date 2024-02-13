5810b299e3326c4368680ab31ec631088f3f4b9b
mourya kumar <kmouryap@gmail.com>
2024-02-14 01:00:25 +0530

    SearchInrange

---
+++ src/Searching/SearchInRange.java
@@ -0,0 +1,31 @@
+package Searching;
+
+public class SearchInRange {
+    public static void main(String[] args) {
+
+        int[] arr = {18 , 12, -7 , 3 , 14 , 28 };
+
+        int target = 3;
+        System.out.println(linersearch(arr,target,1,4));
+    }
+    static int linersearch(int[] arr , int target,int start , int end)
+    {
+        if(arr.length == 0)
+        {
+            return -1;
+
+        }
+        for(int index = start ; index < end;index++)
+        {
+            int element = arr[index];
+
+            if(element == target)
+            {
+                return index;
+            }
+        }
+
+
+        return -1;
+    }
+}
