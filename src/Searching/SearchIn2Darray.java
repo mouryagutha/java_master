bfd5d17e386d4309d79e995e98b04ab844ec3b81
mourya kumar <kmouryap@gmail.com>
2024-02-14 01:26:55 +0530

    Searchin2d

---
+++ src/Searching/SearchIn2Darray.java
@@ -0,0 +1,31 @@
+package Searching;
+
+import java.util.Arrays;
+
+public class SearchIn2Darray {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {23, 4, 5},
+                {18,34,5},{24,56,2},{23,56,1,6,8}
+        };
+        int target = 34;
+        int[] ans = search(arr,target);
+        System.out.println(Arrays.toString(ans));
+    }
+    static int[] search(int[][] arr , int target)
+    {
+        for(int row = 0; row<arr.length;row++){
+            for(int col = 0 ; col<arr[row].length;col++)
+            {
+                if(arr[row][col]==target)
+                {
+                    return  new int[]{row,col};
+                }
+            }
+        }
+
+        return  new int[]{-1,-1};
+    }
+
+
+}
