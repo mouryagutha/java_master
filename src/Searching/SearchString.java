2fff7b6c3b57df1104771f1458798aba939ac0a9
mourya kumar <kmouryap@gmail.com>
2024-02-13 23:17:22 +0530

    liner search-2

---
+++ src/Searching/SearchString.java
@@ -0,0 +1,28 @@
+package Searching;
+
+public class SearchString {
+    public static void main(String[] args) {
+        String name  = "mourya";
+        char target = 'o';
+        System.out.println(search(name,target));
+
+    }
+
+    static  boolean search(String str, char target)
+    {
+        if(str.length() == 0)
+        {
+            return false;
+        }
+        for (int i = 0; i <str.length();i++)
+        {
+            if(target == str.charAt(i))
+            {
+                return true;
+
+            }
+        }
+        return false;
+    }
+
+}
