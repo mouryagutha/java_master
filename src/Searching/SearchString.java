2ca76b9aa333bbd375e789dd33596a4326c93a37
mourya kumar <kmouryap@gmail.com>
2024-02-13 23:21:26 +0530

    string search

--- src/Searching/SearchString.java
+++ src/Searching/SearchString.java
@@ -1,11 +1,30 @@
 package Searching;
 
+import java.util.Arrays;
+
 public class SearchString {
     public static void main(String[] args) {
         String name  = "mourya";
         char target = 'o';
         System.out.println(search(name,target));
+        System.out.println(Arrays.toString(name.toCharArray()));
 
+    }
+
+    static  boolean search2(String str, char target)
+    {
+        if(str.length() == 0)
+        {
+            return false;
+        }
+        for(char ch : str.toCharArray())
+        {
+            if(ch == target)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     static  boolean search(String str, char target)
