2130f058345b7d82b7ef4724bcf4fb3e5abe6994
java_code <kmouryap@gmail.com>
2024-05-12 11:26:05 +0530

    arraylist

--- src/Collections_framworks/Collention.java
+++ src/Collections_framworks/Collention.java
@@ -1,2 +1,34 @@
-package Collections_framworks;public class Collention {
+package Collections_framworks;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Collention {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> al = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+
+        int numbers = 10;
+        System.out.println("Enter the 10 numbers");
+        for(int i= 0 ; i<numbers;i++)
+        {
+            int value = sc.nextInt();
+            al.add(value);
+        }
+
+        System.out.println(al);
+        al.set(4,3);
+        System.out.println(al);
+        int size = al.size();
+        System.out.println("size of array"+size);
+        Object al2 = al.clone();
+        System.out.println("clone object"+al2);
+        al2.equals(al);
+        al.removeAll(al);
+        System.out.println("Size of array is -->"+al);
+
+
+    }
+
 }
