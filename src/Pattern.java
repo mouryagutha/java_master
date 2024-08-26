8f5d7f90724bc143a6c11c65d8e13f2595028057
java_code <kmouryap@gmail.com>
2024-08-27 01:30:04 +0530

    patterns

--- src/Pattern.java
+++ src/Pattern.java
@@ -2,6 +2,10 @@ public class Pattern {
     public static void main(String[] args) {
 
         pattern1(5);
+
+        System.out.println( );
+//        pattern2(5);
+        pattern3(5);
     }
         static void pattern1(int n )
         {
@@ -10,10 +14,39 @@ public class Pattern {
                 for(int j=1; j<=i;j++)
                 {
                     System.out.print("*");
+                    
                 }
                 System.out.println( );
             }
         }
+
+
+
+//    static void pattern2(int n )
+//    {
+//        for(int i =0; i<=n;i--)
+//        {
+//            for(int j=n; j<=i-1;j++)
+//            {
+//                System.out.print("*");
+//
+//            }
+//            System.out.println( );
+//        }
+//    }
+
+    static void pattern3(int n )
+    {
+        for(int i =n; i>=1; --i)
+        {
+            for(int j=1; j<=i;++j)
+            {
+                System.out.print("*");
+
+            }
+            System.out.println( );
+        }
+    }
 
 
 }
