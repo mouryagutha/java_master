4cbb93d73914607c66a1f73f33bef69dd1059ac4
java_code <kmouryap@gmail.com>
2024-09-17 01:16:25 +0530

    java patterns

---
+++ src/Patterns/Pattern5.java
@@ -0,0 +1,31 @@
+package Patterns;
+
+public class Pattern5 {
+    public static void main(String[] args) {
+        int n = 5 ;
+        for(int i = 1 ; i<n ; i++)
+        {
+            for(int j= i ; j<n;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j = 1 ; j<=(2*i-1);j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println( );
+        }
+        for (int i = n-1 ; i>=1;i--)
+        {
+            for(int j = i;j<n;j++)
+            {
+                System.out.print(" ");
+            }
+            for (int j = 1;j<=(2*i-1);j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println( );
+        }
+    }
+}
