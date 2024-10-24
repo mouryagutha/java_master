2239aa6ce656c84aed7737254bf675373cd22de3
java_code <kmouryap@gmail.com>
2024-10-24 11:21:00 +0530

    single number returns

---
+++ src/arrays/Single_number.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class Single_number {
+    public static void main(String[] args) {
+
+        int[] nums= {2,2,1};
+        System.out.println(Single_no(nums));
+    }
+
+    public static int Single_no(int[] nums)
+    {
+        int n = nums.length;
+        int value = 0;
+        for(int i=0; i<n; i++)
+        {
+            value = value^nums[i];
+        }
+        return value;
+    }
+}
