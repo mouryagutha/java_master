530531f77452161b8b808cbd01d58347654cfbad
java_code <kmouryap@gmail.com>
2024-10-26 18:34:20 +0530

    two sum

---
+++ src/arrays/Two_sum.java
@@ -0,0 +1,26 @@
+package arrays;
+
+import java.util.HashMap;
+import java.util.Arrays;
+
+public class Two_sum {
+    public static void main(String[] args) {
+
+        int[] nums = {2, 7, 13, 11};
+        System.out.println(Arrays.toString(twoSum(nums,9)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> numToIndex = new HashMap<>();
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (numToIndex.containsKey(target - nums[i]))
+                return new int[] {numToIndex.get(target - nums[i]), i};
+            numToIndex.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException();
+    }
+}
+
+
